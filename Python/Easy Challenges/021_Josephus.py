8d973499e572b4160fb953cf836d1831d2e9ee9f
Dagan Orton <39224697+CrudeXD@users.noreply.github.com>
2018-05-23 16:15:19 +0200

    Completed Josephus problem, forgot about it.

--- Python/Easy Challenges/021_Josephus.py
+++ Python/Easy Challenges/021_Josephus.py
@@ -1,17 +1,15 @@
 
 
-def exe(people,stPoint,skip):
-    cur = 0 #current position
+def exe(people, stPoint, skip):
+    cur = 0  # current position
     y = []
     for i in range(people):
         y.append(i)
-    while len(y) > 1:
-        #for i in range(cur,len(y)):
-            cur = ((cur + skip) % people)
-            print(str(cur) +' '+str(people))
-            #del y[cur]
-            y.pop(cur)
+    while len(y) > 0:
+        print('cur: {}, People left: {}'.format(cur, y))
+        cur = ((cur + skip) % len(y))
+        y.pop(cur)
     return y
 
 
-print(exe(6,0,2))
+print(exe(6, 0, 2))
